8600f8afb434e2dbbb1655823e562dd749e94256
babanigit <aniketpanchal07@gmail.com>
2024-07-18 23:01:12 +0530

    done with some files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'feature1', loadChildren: () => import('./feature1/feature1.module').then(m => m.Feature1Module) },
-  { path: 'feature2', loadChildren: () => import('./feature2/feature2.module').then(m => m.Feature2Module) },
 
   { path: 'verdict', loadChildren: () => import('./pages/verdictpage/verdictpage.module').then(m => m.VerdictpageModule) },
   { path: 'switcher', loadChildren: () => import('./pages/switcherpage/switcherpage.module').then(m => m.SwitcherpageModule) },
